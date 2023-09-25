5f7a771fd91ecfaf227f3755e5f69bb899ea32f6
qmatrix123 <qmatrix123@163.com>
2023-09-25 23:21:16 +0800

    Sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,13 +4,24 @@ import { Button } from '@/components/ui/button'
 import { Menu } from 'lucide-react'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import Sidebar from '@/components/sidebar'
+import { useEffect, useState } from 'react'
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return null
+    }
+
     return (
         <Sheet>
             <SheetTrigger>
                 <Button variant="ghost" size="icon" className="md:hidden">
-                    <Menu></Menu>
+                    <Menu />
                 </Button>
             </SheetTrigger>
             <SheetContent side="left" className="p-0">
