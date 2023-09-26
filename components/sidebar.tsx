a2e5b202fd2c182c74061818fd755b199cab256a
qmatrix123 <qmatrix123@163.com>
2023-09-26 22:53:40 +0800

    dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 import { Montserrat } from 'next/font/google'
 import { cn } from '@/lib/utils'
@@ -62,6 +63,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname()
     return (
         <div
             className="space-y-4 py-4 flex flex-col h-full
@@ -91,11 +93,12 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full 
-                            justify-start font-medium cursor-pointer
-                            hover:text-white hover:bg-white/10 rounded-lg
-                            transition 
-                            "
+                            className={cn(
+                                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                                pathname === route.href
+                                    ? 'text-white bg-white/10'
+                                    : 'text-zinc-400'
+                            )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon
